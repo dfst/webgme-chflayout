2a76f3ce895271771077c88dbf5ab4bd32fd12d3
Brian Broll <brian.broll@gmail.com>
2016-03-07 15:59:32 -0600

    Set ModelEditor to center panel for easier testing

--- config/config.default.js
+++ config/config.default.js
@@ -1,3 +1,4 @@
+// jshint node: true
 'use strict';
 
 var config = require('./config.webgme'),
@@ -6,6 +7,8 @@ var config = require('./config.webgme'),
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+
+config.visualization.layout.default = 'CHFLayout';
 
 validateConfig(config);
 module.exports = config;
